ed39d027c6ecad80c22276f9de23c8af32245d7a
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-03 17:02:25 -0500

    add validations for username, password, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :email, format: { with: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/ }, uniqueness: true
+  validates :username, :password, presence: true
   has_many :recipes
   has_many :articles
   has_secure_password
