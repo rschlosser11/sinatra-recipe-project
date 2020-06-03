60033e797be6da6fcf5fb2d5b69141fc5d508a31
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-03 14:13:53 -0500

    added html to recipe show view, fixed issue with model relationships between RecipesIngredient, Recipe, and Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ActiveRecord::Base
-  has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients
+  has_many :recipes_ingredients
+  has_many :recipes, through: :recipes_ingredients
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
-  has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
+  has_many :recipes_ingredients
+  has_many :ingredients, through: :recipes_ingredients
   belongs_to :user
   belongs_to :article
 end
