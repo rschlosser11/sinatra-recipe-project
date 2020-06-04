fc10db2e565a9ded497df2e2b6a4cc4b3a374d4c
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-04 09:23:02 -0500

    add edit button to article show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,33 @@
 class ArticlesController < ApplicationController
+# SHOW all articles
   get "/articles" do
     erb :'articles/index'
   end
-
+# SHOW form to create new article
   get "/articles/new" do
     erb :'articles/new'
   end
+# CREATE new article
+  post "/articles" do
+    @article = Article.create(params)
+    @article.user = User.find(session[:user_id])
 
+    redirect "/articles/#{@article.id}"
+  end
+# SHOW individual article
   get "/articles/:id" do
     @article = Article.find(params[:id])
 
     erb :'articles/show'
   end
-
+# DELETE individual article
   delete "/articles/:id" do
     @article = Article.find(params[:id])
     @article.delete
-    
+
     redirect '/articles'
   end
-
+# SHOW edit form for individual article 
   get "/articles/:id/edit" do
     @article = Article.find(params[:id])
 
