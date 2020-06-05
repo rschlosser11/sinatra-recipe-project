e49f91478332e2481170c8148c5feb2ed425f9ca
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-04 19:55:47 -0500

    add patch path to edit a recipe to  the recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -44,4 +44,28 @@ class RecipesController < ApplicationController
 
     erb :'recipes/edit'
   end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    ingredients = params[:ingredients].split(/(,|\r\n)/).delete_if {|string| string == "\r\n" || string == ","}
+    ingredients.each do |ingredient|
+      split_ingredient = ingredient.split(' of ')
+      ingredient_name = split_ingredient.last.chomp.downcase
+      recipe_ingredient = Ingredient.find_by(name: ingredient_name)
+      ingredient_amount = split_ingredient.first.chomp
+      if recipe_ingredient
+        recipe_ingredient = RecipesIngredient.find_by(recipe: @recipe, ingredient: recipe_ingredient)
+        if recipe_ingredient
+          recipe_ingredient
+        else
+          RecipesIngredient.create(amount: ingredient_amount, recipe: @recipe, ingredient:recipe_ingredient)
+        end
+      else
+        new_ingredient = Ingredient.create(name: ingredient_name)
+        RecipesIngredient.create(amount: ingredient_amount, recipe: @recipe, ingredient: new_ingredient)
+      end
+    end
+    @recipe.update(params[:recipe])
+    redirect "/recipes/#{@recipe.id}"
+  end
 end
