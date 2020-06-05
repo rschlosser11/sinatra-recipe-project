9ffa238f277144bcb128100967a5eb08de8f29ae
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-04 21:15:55 -0500

    add post /login route to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,5 +31,15 @@ class ApplicationController < Sinatra::Base
     erb :login
   end
 
+  post "/login" do
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/recipes'
+    else
+      redirect '/login'
+    end
+  end
 
 end
