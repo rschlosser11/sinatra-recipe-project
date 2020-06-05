7197d62633f394c1e79a5b776bfed2af54ea07a8
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-04 21:11:43 -0500

    add post /signup route to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,19 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
+  post "/signup" do
+    user = User.create(params)
+    session[:user_id] = user.id
+    if user.id
+      redirect '/recipes'
+    else
+      redirect '/signup'
+    end
+  end
+
   get "/login" do
     erb :login
   end
+
 
 end
