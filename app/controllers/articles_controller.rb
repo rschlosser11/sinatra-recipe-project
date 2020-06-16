94ee9fa5a6614ad73866864928cb01c2de97fc21
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-16 14:22:05 -0500

    remove comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-# SHOW all articles
+
   get "/articles" do
     if session[:user_id]
       erb :'articles/index'
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
       erb :homepage
     end
   end
-# SHOW form to create new article
+
   get "/articles/new" do
     if session[:user_id]
       erb :'articles/new'
@@ -17,14 +17,14 @@ class ArticlesController < ApplicationController
       erb :homepage
     end
   end
-# CREATE new article
+
   post "/articles" do
     @article = Article.new(params)
     @article.user = User.find(session[:user_id])
     @article.save
     redirect "/articles/#{@article.id}"
   end
-# SHOW individual article
+
   get "/articles/:id" do
     if session[:user_id]
       @article = Article.find(params[:id])
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
       erb :homepage
     end
   end
-# DELETE individual article
+
   delete "/articles/:id" do
     @article = Article.find(params[:id])
     if session[:user_id] == @article.user.id
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
       erb :'/articles/:id'
     end
   end
-# SHOW edit form for individual article
+
   get "/articles/:id/edit" do
     @article = Article.find(params[:id])
     if session[:user_id] == @article.user.id
